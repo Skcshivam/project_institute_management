035ec1116023e0eab6f5387850f44ce95e9e2a53
Skcshivam <shichoudhary999@gmail.com>
2025-06-16 17:12:35 +0530

    sign up and login complete with testing

--- API/routes/user.js
+++ API/routes/user.js
@@ -1,11 +1,102 @@
 const express = require("express");
 const router = express.Router();
+const cloudinary = require("cloudinary").v2;
+require("dotenv").config();
+const User = require("../model/User");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
 
 //signup
 router.post("/signup", (req, res) => {
-  console.log(req);
-  res.status(200).json({
-    msg: "signup request",
+  User.find({ email: req.body.email }).then((users) => {
+    if (users.length > 0) {
+      return res.status(500).json({
+        error: "Email already registered",
+      });
+    }
+
+    cloudinary.uploader.upload(req.files.image.tempFilePath, (err, result) => {
+      //console.log(result);
+
+      bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) {
+          return res.status(500).json({
+            error: err,
+          });
+        }
+        const newUser = new User({
+          _id: new mongoose.Types.ObjectId(),
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          email: req.body.email,
+          password: hash,
+          imageUrl: result.secure_url,
+          imageId: result.public_id,
+        });
+        newUser
+          .save()
+          .then((result) => {
+            res.status(200).json({
+              newStudent: result,
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+              error: err,
+            });
+          });
+      });
+    });
+  });
+});
+
+//login
+router.post("/login", (req, res) => {
+  User.find({ email: req.body.email }).then((users) => {
+    console.log(User[0]);
+    if (User.length == 0) {
+      return res.status(500).json({
+        msg: "email not register",
+      });
+    }
+    bcrypt.compare(req.body.password, users[0].password, (err, result) => {
+      if (!result) {
+        return res.status(500).json({
+          error: "password matching failed ...",
+        });
+      }
+
+      const token = jwt.sign(
+        {
+          email: users[0].email,
+          firstName: users[0].firstName,
+          lastName: users[0].lastName,
+          uId: users[0]._id,
+        },
+        "sbs online classes 123",
+        {
+          expiresIn: "365d",
+        }
+      );
+
+      res.status(200).json({
+        _id: users[0]._id,
+        firstName: users[0].firstName,
+        lastName: users[0].lastName,
+        email: users[0].email,
+        imageUrl: users[0].imageUrl,
+        imageId: users[0].imageId,
+        token: token,
+      });
+    });
   });
 });
 
